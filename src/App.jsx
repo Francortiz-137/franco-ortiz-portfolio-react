1d93aa5873768625fd88fa15d8acfa8d9feb2cc2
Francortiz-137 <francortiz.c@gmail.com>
2023-12-15 16:20:27 -0300

    project setup + Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { NavBar } from './components/NavBar';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+    <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
